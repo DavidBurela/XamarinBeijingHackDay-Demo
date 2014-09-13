41680daef74cc94d7717155f16c593e5ffdb77c2
David Burela <David.Burela@gmail.com>
2014-09-13 13:45:19 +0800

    Cleaned the BetterViewModel

--- XamarinBeijingApp/XamarinBeijingApp/ViewModels/BetterViewModel.cs
+++ XamarinBeijingApp/XamarinBeijingApp/ViewModels/BetterViewModel.cs
@@ -1,26 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace XamarinBeijingApp.ViewModels
 {
-    class BetterViewModel : INotifyPropertyChanged
+    internal class BetterViewModel : INotifyPropertyChanged
     {
+        private string _countText;
+        private string _greeting;
+        private bool _isBusy;
+
+        public BetterViewModel()
+        {
+            IsBusy = false;
+            Count = 0;
+            CountText = "You have clicked " + Count + " times";
+            Greeting = "you have not entered your name yet.";
+
+            CountCommand = new Command(CountBy1);
+            SayHelloCommand = new Command(SayHello);
+
+            Animals = new ObservableCollection<Animal>
+            {
+                new Animal {Name = "Koala", Color = "Grey", Age = "1"},
+                new Animal {Name = "Cat", Color = "Orange", Age = "5"},
+                new Animal {Name = "Panda", Color = "Black", Age = "8"}
+            };
+        }
+
         public ObservableCollection<Animal> Animals { get; set; }
         public ICommand CountCommand { get; set; }
         public ICommand SayHelloCommand { get; set; }
 
         public int Count { get; set; }
-
-        private string _countText;
-        private string _greeting;
-        private bool _isBusy;
 
         public string CountText
         {
@@ -44,28 +58,11 @@ namespace XamarinBeijingApp.ViewModels
             }
         }
 
-        public BetterViewModel()
-        {
-            IsBusy = false;
-            Count = 0;
-            CountText = "You have clicked " + Count + " times";
-            Greeting = "you have not entered your name yet.";
-
-            CountCommand = new Command(CountBy1);
-            SayHelloCommand = new Command(SayHello);
-
-            Animals = new ObservableCollection<Animal>
-            {
-                new Animal{Name = "Koala", Color = "Grey", Age = "1"},
-                new Animal{Name="Cat", Color = "Orange", Age = "5"},
-                new Animal{Name="Panda", Color = "Black", Age = "8"}
-            };
-        }
-
         public bool NotIsBusy
         {
             get { return !IsBusy; }
         }
+
         public bool IsBusy
         {
             get { return _isBusy; }
@@ -77,13 +74,15 @@ namespace XamarinBeijingApp.ViewModels
             }
         }
 
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public async void CountBy1()
         {
             // Start doing a lot of work
             // Show the Busy indicator
             IsBusy = true;
 
-            await Task.Delay(3000);
+            await Task.Delay(3000); // sleep for 3 seconds
             Count++;
             CountText = "You have clicked " + Count + " times";
 
@@ -98,10 +97,9 @@ namespace XamarinBeijingApp.ViewModels
 
         public void OnPropertyChanged(string propertyName)
         {
-            if(PropertyChanged != null)
+            if (PropertyChanged != null)
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
         }
-        public event PropertyChangedEventHandler PropertyChanged;
     }
 
     public class Animal
